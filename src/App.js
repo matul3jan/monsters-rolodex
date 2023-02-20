48c07fcb056594171daa6f65161e58e4a9424a1e
Matul Jain <matul3jan@gmail.com>
2023-02-20 22:02:15 +0000

    Class based components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      search: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState(() => { return { monsters: users } }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState(() => { return { search: event.target.value.toLowerCase() } });
+  }
+
+  render() {
+    const { monsters, search } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter(m => m.name.toLowerCase().includes(search));
+    return (
+      <div className='App'>
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox onChangeHandler={onSearchChange} placeholder="search monster" className="monster-search-box" />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
